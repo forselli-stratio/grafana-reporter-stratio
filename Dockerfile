8a9d1d092c7a667a0efb7df1d6521764fba7554b
forselli-stratio <forselli@stratio.com>
2019-09-03 12:37:37 +0200

    Added vars, other packages, deployment json and the way that muttrc copies to its directory.

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,13 @@ FROM izakmarais/grafana-reporter:2.2.1
 MAINTAINER Stratio Operations <operations@stratio.com>
 
 ADD entrypoint.sh /entrypoint.sh
-ADD muttrc ~/.mutt/muttrc
+ADD muttrc /muttrc
 
 RUN apt-get update && \
-    apt-get install jq telnet curl wget procps mutt -y && \
+    apt-get install jq telnet curl wget procps mutt gpgsm vim strace -y && \
     mkdir -p ~/.mutt/cache/headers && \
     mkdir ~/.mutt/cache/bodies && \
-    touch ~/.mutt/certificates
+    touch ~/.mutt/certificates && \
+    mv /muttrc ~/.mutt/
 
 ENTRYPOINT [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-EXPORT GRAFANA_URL=172.19.2.34:3000
-EXPORT DASHBOARD_ID=-CC1XCKZz
-EXPORT API_KEY=eyJrIjoiOTJDUUJ3MmMxRm1GRjV0TEZYeENTTmFEVmVNbTc4dlUiLCJuIjoicHJ1ZWJhIiwiaWQiOjF9
-
-/usr/local/bin/grafana-reporter -ip $GRAFANA_URL &
+/usr/local/bin/grafana-reporter -ip $GRAFANA_URL -proto $PROTO -ssl-check=$SSL_CHECK &
 
 wget -O /tmp/informe.pdf "localhost:8686/api/v5/report/$DASHBOARD_ID?apitoken=$API_KEY"
 
-sleep 40
+sleep 500
 
-mutt -s 'Prueba Informe' forselli@stratio.com -a /tmp/informe.pdf < /dev/null
+mutt -s 'Informe Semanal' forselli@stratio.com -a /tmp/informe.pdf < /dev/null
